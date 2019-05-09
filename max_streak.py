7cefa2f50f29f444a357c0a48330a7da25a78c22
Olivia Ea <ea.olivia1@gmail.com>
2019-05-08 18:51:36 -0700

    Max streak

---
+++ max_streak.py
@@ -0,0 +1,18 @@
+def max_streak(num_employees, data):
+
+    full_attend = 'Y' * num_employees
+    current = 0
+    count = 0
+    
+    for day in data:
+        if day == full_attend:
+            current += 1
+        else:
+            count = max(count, current)
+            current_streak = 0
+    return max(count, current_streak)
+
+
+
+
+
